8cfd11d782388763523bef78a99ba5916099dacd
Sumit <sk7627577@gmail.com>
2024-01-03 10:28:49 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
+import "./App.css";
+import "./Style/Main.css";
+import "./Style/Responsive.css";
+import { Route, Routes } from "react-router-dom";
 
-import './App.css';
-import './Style/Main.css'
-import './Style/Responsive.css'
-import { Route, Routes } from 'react-router-dom';
-
-import Navigation from './Component/Navigation'
-import About from './Pages/About Page/About'
-import Footer from './Component/Footer';
-import Home from './Pages/Home Page/Home';
-import Contact from './Pages/Contact Page/Contact';
-import LandingPage2 from './Pages/LandingPage2/LandingPage2';
+import Navigation from "./Component/Navigation";
+import About from "./Pages/About Page/About";
+import Footer from "./Component/Footer";
+import Home from "./Pages/Home Page/Home";
+import Contact from "./Pages/Contact Page/Contact";
+import LandingPage2 from "./Pages/LandingPage2/LandingPage2";
+import Blogs from "./Pages/Blogs/Blogs";
 function App() {
   return (
     <>
-
-      <LandingPage2 />
-      {/* <Navigation />
+      {/* <LandingPage2 /> */}
+      <Navigation />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/about-page" exact element={<About />} />
+        {/* Industries We Serve */}
+        <Route
+          path="/industries-we-serve/hotel-marketing"
+          exact
+          element={<Blogs />}
+        />
+        <Route
+          path="/industries-we-serve/restaurant-marketing"
+          exact
+          element={<Blogs />}
+        />
+        <Route
+          path="/industries-we-serve/cloud-kithen-marketing"
+          exact
+          element={<Blogs />}
+        />
+        {/*Services*/}
+        <Route path="/services/website-development" exact element={<Blogs />} />
+        <Route
+          path="/services/social-media-marketing"
+          exact
+          element={<Blogs />}
+        />
+        <Route
+          path="/services/serach-engine-optimization"
+          exact
+          element={<Blogs />}
+        />
+        <Route path="/services/paid-ad-campaigns" exact element={<Blogs />} />
+        <Route path="/blog-page" exact element={<Blogs />} />
+
+        <Route path="/blog-page" exact element={<Blogs />} />
         <Route path="/contact-page" exact element={<Contact />} />
-        <Route path="/landing-page2" exact element={<LandingPage2/>} />
+        <Route path="/landing-page2" exact element={<LandingPage2 />} />
       </Routes>
-      <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/Component/Navigation.jsx
+++ src/Component/Navigation.jsx
@@ -17,22 +17,22 @@ const Navigation = () => {
               <Nav className="me-auto mid_nav">
                 <Nav.Link href="/about-page">About</Nav.Link><span>&#124;</span>
                 <NavDropdown title="Industries We Serve" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Hotel Marketing</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Restaurant Marketing</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Cloud Kithen Marketing</NavDropdown.Item>
+                  <NavDropdown.Item href="/industries-we-serve/hotel-marketing">Hotel Marketing</NavDropdown.Item>
+                  <NavDropdown.Item href="/industries-we-serve/restaurant-marketing">Restaurant Marketing</NavDropdown.Item>
+                  <NavDropdown.Item href="/industries-we-serve/cloud-kithen-marketing">Cloud Kithen Marketing</NavDropdown.Item>
                 </NavDropdown><span>&#124;</span>
                 <NavDropdown title="Services" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Website Development</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Social Media Marketing</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Serach Engine Optimization(SEO)</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Paid Ad Campaigns</NavDropdown.Item>
+                  <NavDropdown.Item href="/services/website-development">Website Development</NavDropdown.Item>
+                  <NavDropdown.Item href="/services/social-media-marketing">Social Media Marketing</NavDropdown.Item>
+                  <NavDropdown.Item href="/services/serach-engine-optimization">Serach Engine Optimization(SEO)</NavDropdown.Item>
+                  <NavDropdown.Item href="/services/paid-ad-campaigns">Paid Ad Campaigns</NavDropdown.Item>
                 </NavDropdown><span>&#124;</span>
-                <Nav.Link href="#link">Blogs</Nav.Link><span>&#124;</span>
+                <Nav.Link href="/blog-page">Blogs</Nav.Link><span>&#124;</span>
                 {/* <Nav.Link href="/landing-page2">Landing Page2</Nav.Link><span>&#124;</span> */}
                 <Nav.Link href="/contact-page">Contact Us</Nav.Link> 
 
               </Nav>
-              <Nav.Link className='navphone' href="#link"><i class="fa-solid fa-phone"></i>+919501868775</Nav.Link>
+              <Nav.Link className='navphone' href="#link"><i className="fa-solid fa-phone"></i>+919501868775</Nav.Link>
 
             </Navbar.Collapse>
           </Container>
