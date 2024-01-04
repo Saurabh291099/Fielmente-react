a4eff567770487270144c1ff78506a8044e3caeb
Sumit <sk7627577@gmail.com>
2024-01-04 15:02:13 +0530

    update

--- src/Pages/About Page/Section1.jsx
+++ src/Pages/About Page/Section1.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Img1 from "../../Assests/Images/WHO-ARE-WE-2.jpg";
-import Img2 from "../../Assests/Images/APPROACH-US-3.jpg";
+import Img2 from "../../Assests/about-us/director-restaurant.png";
+import Img3 from "../../Assests/about-us/aq1-1.png";
 import { Link } from "react-router-dom";
 import { animated, useSpring } from "@react-spring/web";
 import "./Aboutuspage.css";
@@ -37,58 +38,93 @@ function Section1() {
     <div>
       <div className="about-us-section-1 border-top container px-2 py-3">
         <div className="container">
-          <div className="row mt-4 pt-5">
-            <div className="col-lg-6 ps-lg-2 pe-lg-4">
-              <div className="about-us-content-wrap py-4">
-                <h2 className="text-uppercase mb-4">{headingLeft}</h2>
-                <p>{descriptionLeft}</p>
-              </div>
-              <div className="img-div">
-                <img src={Img2} className="img-fluid rounded-3" alt="" />
+          
+          <div className="row mt-lg-4 pt-lg-5 flex-column">
+            <div className="col">
+              <div className="row">
+                <div className="col-lg-6">
+                  <div className="about-us-content-wrap">
+                    <h2 className="text-uppercase mb-4">{headingLeft}</h2>
+                    <p>{descriptionLeft}</p>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="img-div">
+                    <img src={Img1} className="img-fluid rounded-3" alt="" />
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="col-lg-6 ps-lg-4 pe-lg-2 mt-3 mt-lg-0">
-              <div className="img-div">
-                <img src={Img1} className="img-fluid rounded-3" alt="" />
+            <div className="col">
+              <div className="row flex-column-reverse flex-lg-row">
+                <div className="col oreder-1">
+                  <div className="img-div">
+                    <img src={Img2} className="img-fluid rounded-3" alt="" />
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="about-us-content-wrap py-lg-3 py-2 oreder-2">
+                    <h2 className="text-uppercase mb-4">{headingRight}</h2>
+                    <p className="mb-1">{descriptionRight}</p>
+                  </div>
+                  <div className="d-none d-sm-none d-block-md d-lg-block">
+                    <div className="buttons-link d-flex oreder-3 align-items-center justify-content-center mb-2">
+                      <Link
+                        to="/contact-page"
+                        className="d-flex align-items-center justify-content-center px-4 py-2 rounded-3"
+                      >
+                        Contact us
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="about-us-content-wrap py-4">
-                <h2 className="text-uppercase mb-4">{headingRight}</h2>
-                <p>{descriptionRight}</p>
-              </div>
-              <div className="buttons-link d-flex align-items-center justify-content-center mb-2">
+            </div>
+            <div className="d-sm-block d-none-md d-lg-none mt-2">
+              <div className="buttons-link d-flex oreder-3 align-items-center justify-content-center mb-2">
                 <Link
                   to="/contact-page"
-                  className="d-flex align-items-center justify-content-center gap-1 px-4 py-2 rounded-3"
+                  className="d-flex align-items-center justify-content-center px-4 py-2 rounded-3"
                 >
                   Contact us
                 </Link>
               </div>
             </div>
           </div>
+          {/* number-section start */}
           <div className="container  py-2 border-bottom">
             <div className="row justify-content-center">
               {numberData.map((item, i) => (
-                <div className="col-lg-4 col-md-6 col-12 p-5" key={i}>
+                <div className="col-lg-4 col-md-6 col-6 p-5" key={i}>
                   <div className="bnr-counter-inr about-number-section text-center">
                     <span className="count-num">
-                      <Number n={parseFloat(item.number)} />{item.data}
+                      <Number n={parseFloat(item.number)} />
+                      {item.data}
                     </span>
-                    <span className="text-uppercase about-number-section-text">{item.text}</span>
+                    <span className="text-uppercase about-number-section-text">
+                      {item.text}
+                    </span>
                   </div>
                 </div>
               ))}
             </div>
           </div>
-          <div className="container py-5 border-bottom">
+          {/* number-section end */}
+          {/* bottom-section-1 start */}
+          <div className="container py-5 ">
             <div className="bottom-content-wrap m-auto p-2">
-              <div className="container about-us-content-wrap text-center p-0">
-                <h2 className="text-uppercase">{heading}</h2>
+              <div className="container text-center p-0">
+                <h2>{heading}</h2>
               </div>
-              <div className="container about-us-content-wrap text-center p-0">
+              <div className="container text-center mt-3 p-0">
                 <p>{description}</p>
               </div>
             </div>
+            <div className="bottom-card-div my-4">
+              <img src={Img3} alt="" />
+            </div>
           </div>
+          {/* bottom-section-1 end */}
         </div>
       </div>
     </div>
